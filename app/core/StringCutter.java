9dd5a64e9c069a52cff559c1a87139cbbe47bd3d
Brian On <58958011+briano-addepar@users.noreply.github.com>
2022-05-11 11:17:39 -0700

    Enhance formatting
    Don't ignore or decline this PR pls

--- app/core/StringCutter.java
+++ app/core/StringCutter.java
@@ -2,8 +2,8 @@ package core;
 
 public class StringCutter {
     public static String cut(String stringToCut) {
-        StringBuilder returnString= new StringBuilder();
-        for (int i = 2; i < stringToCut.length(); i= i+3) {
+        StringBuilder returnString = new StringBuilder();
+        for (int i = 2; i < stringToCut.length(); i = i + 3) {
             returnString.append(stringToCut.charAt(i));
         }
         return returnString.toString();
